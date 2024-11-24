886345fdff1d845cacd8fbcf5827dc7c00b95776
Miguel Sanchez <miguel.sdguez@gmail.com>
2024-11-24 10:47:37 +0100

    Minor changes for run

--- train_slurm.sh
+++ train_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##----------------------- Start job description -----------------------
-#SBATCH --job-name=0_STTN_inpainting
+#SBATCH --job-name=1_STTN_inpainting
 #SBATCH --nodes=1
 #SBATCH --nodelist=n008
 #SBATCH --ntasks=1                 # number of MPI processes
@@ -12,13 +12,21 @@
 #SBATCH --time=96:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=miguel.sanchez.dominguez@upm.es
-#SBATCH --output=slurm_STTN2.out
+#SBATCH --output=slurm_STTN_sch.out
 ##------------------------ End job description ------------------------
 
 date
 hostname
 eval "$(conda shell.bash hook)"
 conda activate AIRML
-python train.py --config configs/twin_jet_old_scaled_vg.json --model sttn
-##python test_custom.py --config configs/twin_jet_old_scaled_vg.json --model sttn --ckpt 4000 --ds_name full_ds_1e4
+##python train.py --config configs/twin_jet_old_scaled_dev.json --model sttn
+##python test_custom.py --config configs/twin_jet_old_scaled_dev.json --model sttn --ckpt 4500 --ds_name full_ds_1e4
+
+python train.py --config configs/scheduled_runs/w5_s5.json --model sttn
+python train.py --config configs/scheduled_runs/w5_s10.json --model sttn
+python train.py --config configs/scheduled_runs/w11_s5.json --model sttn
+python train.py --config configs/scheduled_runs/w11_s10.json --model sttn
+python train.py --config configs/scheduled_runs/w21_s5.json --model sttn
+python train.py --config configs/scheduled_runs/w21_s10.json --model sttn
+
 date
